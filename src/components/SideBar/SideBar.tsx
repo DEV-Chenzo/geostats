753946a967d587e64b4ba24a3b772d037cc39464
DEV-Chenzo <vicenzowebdeveloper@gmail.com>
2026-07-17 12:39:09 -0300

    style: Update sidebar
    padding-y increased

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -30,7 +30,7 @@ export default function SideBar() {
             <p className="text-xs">Clientes</p>
           </div>
         </button>
-        <button className="w-full flex items-center justify-items-start hover:bg-primary-600 hover:text-tertiary-100  text-tertiary-200  py-2 px-5 rounded">
+        <button className="w-full flex items-center justify-items-start hover:bg-primary-600 hover:text-tertiary-100  text-tertiary-200  py-3 px-5 rounded">
           <div className="flex flex-row items-center gap-4 w-35">
             <MdOutlineShoppingCart className="text-2xl" />{" "}
             <p className="text-xs">Vendas</p>
